a22808e173cfcc62d6c6ca7581cb20e55772be85
GardyVoir <wilfart.sarah@gmail.com>
2022-12-15 13:44:01 +0100

    working

--- app.js
+++ app.js
@@ -1,65 +1,61 @@
-// Import required AWS SDK clients and commands for Node.js.
 import { PutObjectCommand, ListObjectsCommand } from "@aws-sdk/client-s3";
 import { s3Client } from "./libs/s3Client.js";
-import fetch from "node-fetch";
-import formidable from 'formidable';
+import formidable from "formidable";
 
-import express from 'express';
+import express from "express";
+import { readFileSync } from "fs"
 const app = express();
-
-app.get("/", (req, res) => {
-    getFiles(function (err, rows) {
-        if (err) {
-            req.flash('error', err)
-            res.render('index', { data: '' })
-        } else {
-            res.render('index', { data: rows })
-        }
-    })
-});
+const bucket = "mds-course-bucket";
+app.set("view engine", "ejs");
 
 app.listen(3000, () => {
-    console.log("server started on http://localhost:3000");
+  console.log("server started on http://localhost:3000");
+});
+
+app.get("/", (req, res) => {
+  Promise.all([getFiles()])
+    .then((rows) => {
+      res.render("index", { data: rows[0] });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
 
 app.post("/fileupload", (req, res) => {
+  try {
     var form = new formidable.IncomingForm();
     form.parse(req, async function (err, fields, files) {
-        const file = files.filetoupload;
-        const params = {
-            Bucket: "mds-cloudcomputing",
-            Key: file.originalFilename,
-            Body: file
-        };
-        try {
-            const results = await s3Client.send(new PutObjectCommand(params));
-            return results; // For unit tests.
-        } catch (err) {
-            console.log("Error", err);
-        }
-    })
-})
+      const file = readFileSync(files.filetoupload.filepath);
+      const params = {
+        Bucket: bucket,
+        Key: files.filetoupload.originalFilename,
+        Body: file,
+      };
+      await s3Client.send(new PutObjectCommand(params));
+      res.redirect("/");
+      res.end();
+    });
+  } catch (err) {
+    console.log("Error", err);    
+    res.redirect("/");
+    res.end();
+  }
+});
+
+app.get("/file/:id", (req, res) => {
+    res.send("Id is set to " + req.params.id);
+  });
 
 const getFiles = async () => {
-    await new Promise(resolve => setTimeout(resolve, 3000));
-    return [
-        {
-          "name": "blabla",
-          "type": "jpg",          
-        },
-        {
-          "name": "truc",
-          "type": "txt",          
-        }
-    ]
-
-    // const params = {
-    //     Bucket: "mds-cloudcomputing"
-    // };
-    // try {
-    //     const results = await s3Client.send(new ListObjectsCommand(params));
-    //     return results;
-    // } catch (err) {
-    //     console.log("Error", err);
-    // }
+  const params = {
+    Bucket: bucket,
+  };
+  try {
+    const results = await s3Client.send(new ListObjectsCommand(params));
+    return results.Contents;
+  } catch (err) {
+    console.log("Error", err);
+  }
 };
--- libs/s3Client.js
+++ libs/s3Client.js
@@ -1,9 +1,9 @@
 import { S3Client } from "@aws-sdk/client-s3";
-import { json } from "node:stream/consumers";
-import credentials from "./.aws/credentials.json" assert {type: 'json'};
-// Set the AWS Region.
-const REGION = "eu-west-3"; //e.g. "us-east-1"
-const CREDENTIALS = ""
-// Create an Amazon S3 service client object.
+//import credentials from "./.aws/credentials.json" assert {type: 'json'};
+const credentials = {
+    accessKeyId : process.env.accessKeyId,
+    secretAccessKey : process.env.secretAccessKey
+}
+const REGION = "eu-west-3";
 const s3Client = new S3Client({ region: REGION, credentials: credentials });
 export { s3Client };
